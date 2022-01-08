856818ba6539dff14442a228c76a0363a4615d89
droa3v <droa3v@gmail.com>
2022-01-08 22:50:40 +0700

    Message styles added

--- src/chat/messages/Message.tsx
+++ src/chat/messages/Message.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import styles from "./Message.module.scss";
 
-export const Message: React.FC = (): JSX.Element => {
-  return <div>Hello world!</div>;
+interface MessageProps {
+  my: boolean;
+  children: React.ReactNode;
+}
+
+export const Message: React.FC<MessageProps> = ({
+  my,
+  children,
+}): JSX.Element => {
+  return (
+    <div className={`${my ? styles.myMessage : styles.inMessage}`}>
+      <div
+        className={`${styles.textContainer} ${
+          my ? styles.tailRight : styles.tailLeft
+        }`}
+      >
+        <p>{children}</p>
+      </div>
+    </div>
+  );
 };
--- src/chat/messages/Messages.tsx
+++ src/chat/messages/Messages.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import { Message } from "./Message";
+import styles from "./Messages.module.scss";
 
 export const Messages: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <Message />
+    <div className={styles.container}>
+      <Message my={true}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        tincidunt eleifend urna eu pellentesque. Pellentesque tempus a tellus
+        condimentum posuere.
+      </Message>
+      <Message my={false}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        tincidunt eleifend urna eu pellentesque. Pellentesque tempus a tellus
+        condimentum posuere.
+      </Message>
+      <Message my={true}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        tincidunt eleifend urna eu pellentesque. Pellentesque tempus a tellus
+        condimentum posuere.
+      </Message>
+      <Message my={false}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        tincidunt eleifend urna eu pellentesque. Pellentesque tempus a tellus
+        condimentum posuere.
+      </Message>
+      <Message my={true}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        tincidunt eleifend urna eu pellentesque. Pellentesque tempus a tellus
+        condimentum posuere.
+      </Message>
+      <Message my={false}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        tincidunt eleifend urna eu pellentesque. Pellentesque tempus a tellus
+        condimentum posuere.
+      </Message>
     </div>
   );
 };
