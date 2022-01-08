f6d6d12242b5a41fea9a8084b435773214dff367
droa3v <droa3v@gmail.com>
2022-01-08 21:26:27 +0700

    Added button styles and animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Chat } from "./chat/Chat";
+import "./scss/App.scss";
 
 export const App: React.FC = (): JSX.Element => {
   return (
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
@@ -7,14 +7,50 @@ interface ButtonProps {
   disabled?: boolean;
 }
 
+interface ICoords {
+  x: number;
+  y: number;
+}
+
 export const Button: React.FC<ButtonProps> = ({
   onClick,
   children,
   disabled,
 }): JSX.Element => {
+  const [coords, setCoords] = useState<ICoords>({ x: -1, y: -1 });
+  const [isRippling, setIsRippling] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (coords.x !== -1 && coords.y !== -1) {
+      setIsRippling(true);
+      setTimeout(() => setIsRippling(false), 300);
+    } else setIsRippling(false);
+  }, [coords]);
+
+  useEffect(() => {
+    if (!isRippling) setCoords({ x: -1, y: -1 });
+  }, [isRippling]);
+
   return (
-    <button onClick={onClick} disabled={disabled} className={styles.btn}>
-      {children}
+    <button
+      onClick={(e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        setCoords({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+        onClick && onClick(e);
+      }}
+      disabled={disabled}
+      className={styles.btn}
+    >
+      {isRippling ? (
+        <span
+          className={styles.ripple}
+          style={{
+            left: coords.x,
+            top: coords.y,
+          }}
+        />
+      ) : null}
+      <span className={styles.content}>{children}</span>
     </button>
   );
 };
