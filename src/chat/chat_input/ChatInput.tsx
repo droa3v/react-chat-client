7d38b12c3cdef09f95747b9936591225506b3935
droa3v <droa3v@gmail.com>
2022-01-10 15:06:38 +0700

    Minor changes to input styles

--- src/chat/chat_input/ChatInput.tsx
+++ src/chat/chat_input/ChatInput.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Button } from "../../components/Button/Button";
+import styles from "./ChatInput.module.scss";
 
 export const ChatInput: React.FC = (): JSX.Element => {
   return (
-    <div>
+    <div className={styles.container}>
       <input
         type="text"
         name="MessageInput"
         placeholder="Type your message here..."
+        className={styles.input}
       />
       <Button>Submit</Button>
     </div>
