0bb541ea49e8b67516f4cc6680497a17150015be
droa3v <droa3v@gmail.com>
2022-01-10 21:01:57 +0700

    Added nav buttons

---
+++ src/menu/menu_content/MenuContent.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./MenuContent.module.scss";
+
+export const MenuContent: React.FC = (): JSX.Element => {
+  return <div className={styles.container}></div>;
+};
