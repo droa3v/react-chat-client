db9f7a03c16685adc46dbb2eefc948971b20a18e
droa3v <droa3v@gmail.com>
2022-01-10 15:29:13 +0700

    Added menu with basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Chat } from "./chat/Chat";
+import { Menu } from "./menu/Menu";
 import "./scss/App.scss";
 
 export const App: React.FC = (): JSX.Element => {
   return (
-    <div>
+    <div className="appContainer">
+      <Menu />
       <Chat />
     </div>
   );
---
+++ src/menu/Menu.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Menu.module.scss";
+
+export const Menu = () => {
+  return <div className={styles.container}>hello</div>;
+};
